dc56a50e5b034d68d77a948e52316c30b39f3a10
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2017-09-20 20:47:15 +0200

    [TECH] Migrating the application definition into a common file for Android and iOS

--- app/App.js
+++ app/App.js
@@ -1,82 +1,32 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
-  Text,
-  View,
-  Button
+  View
 } from 'react-native';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as Actions from './actions'
+import RootComponent from './RootComponent';
 
-class App extends Component {
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-  // constructor(props) {
-  //   super(props);
-  //   console.log(props)
-  //   this.actions = props.actions;
-  // }
+import reducers from './reducers';
 
-  onLogin() {
-    console.log(this.props.actions.login('alwin'))
-    //console.log(this.props.login('SALUT'))
-  }
+const store = createStore(reducers)
 
+export default class App extends Component {
   render() {
-    console.log('------------------------')
-    console.log(this.props.state.user.isLoggedIn)
-    console.log('------------------------')
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, you can simply edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Button
-          onPress={this.onLogin.bind(this)}
-          title="Login"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Text>
-           {this.props.state.user.isLoggedIn}
-        </Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <RootComponent />
+        </View>
+      </Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
-
-const mapStateToProps = state => ({state})
-const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(Actions, dispatch)})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
