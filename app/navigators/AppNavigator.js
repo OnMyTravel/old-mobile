17ecfc26508bf767b1f188cc95465148c2c53927
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2017-09-24 19:52:14 +0200

    Reorganizing the files

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 
-import LoginScreen from '../components/LoginScreen';
-import MainScreen from '../components/MainScreen';
-import ProfileScreen from '../components/ProfileScreen';
+import LoginScreen from '../screens/LoginScreen';
+import MainScreen from '../screens/MainScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 export const AppNavigator = StackNavigator({
   Login: { screen: LoginScreen },
