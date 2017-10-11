740c8097e4ccff69598fa2152b716c8f482e69f8
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2017-10-11 21:49:19 +0200

    Setting up the redux-persist to save the application state

--- app/App.js
+++ app/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist'
+import { AsyncStorage } from 'react-native'
 
 import AppWithNavigationState from './navigators/AppNavigator';
 
 import reducers from './reducers';
 
-const store = createStore(reducers);
+
+// const store = createStore(reducers);
+const store = createStore(reducers, undefined, autoRehydrate());
+persistStore(store, { storage: AsyncStorage });
 
 export default class App extends Component {
   render() {
